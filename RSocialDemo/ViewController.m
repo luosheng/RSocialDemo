962acbcf4740cd9e880a1d27e4451e089794d26c
Alex Rezit <AlexRezit@gmail.com>
2013-01-16 23:17:12 +0800

    Add view.

--- RSocialDemo/ViewController.m
+++ RSocialDemo/ViewController.m
@@ -6,18 +6,67 @@
 //  Copyright (c) 2013 Seymour Dev. All rights reserved.
 //
 
+#import "RDoubanAuth.h"
 #import "ViewController.h"
 
 @interface ViewController ()
+
+@property (nonatomic, strong) IBOutlet UILabel *statusLabel;
+
+@property (nonatomic, strong) RDoubanAuth *doubanAuth;
+
+- (IBAction)checkButtonPressed:(UIButton *)button;
+- (IBAction)loginButtonPressed:(UIButton *)button;
+- (IBAction)logoutButtonPressed:(UIButton *)button;
 
 @end
 
 @implementation ViewController
 
+- (void)checkButtonPressed:(UIButton *)button
+{
+    self.statusLabel.text = NSLocalizedString(@"Checking", nil);
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        BOOL isAuthed = [self.doubanAuth checkAuthorizationUpdate];
+        dispatch_async(dispatch_get_main_queue(), ^{
+            self.statusLabel.text = isAuthed ? NSLocalizedString(@"Authed", nil) : NSLocalizedString(@"Not Authed", nil);
+        });
+    });
+}
+
+- (void)loginButtonPressed:(UIButton *)button
+{
+    [self.doubanAuth authorizeWithCompletionHandler:^(BOOL success) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            self.statusLabel.text = success ? NSLocalizedString(@"Authed", nil) : NSLocalizedString(@"Not Authed", nil);
+        });
+    }];
+}
+
+- (void)logoutButtonPressed:(UIButton *)button
+{
+    [self.doubanAuth logout];
+    self.statusLabel.text = self.doubanAuth.isAuthorized ? NSLocalizedString(@"Authed", nil) : NSLocalizedString(@"Not Authed", nil);
+}
+
+#pragma mark - Life cycle
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    RDoubanAuth *doubanAuth = [[[RDoubanAuth alloc] init] autorelease];
+    doubanAuth.clientID = @"0bdb1a1a76c3e40b23914e4f644bac21";
+    doubanAuth.clientSecret = @"c92655b1f2bea687";
+    doubanAuth.redirectURI = @"rsocial://";
+    self.doubanAuth = doubanAuth;
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+    self.statusLabel.text = self.doubanAuth.isAuthorized ? NSLocalizedString(@"Authed", nil) : NSLocalizedString(@"Not Authed", nil);
 }
 
 - (void)didReceiveMemoryWarning
